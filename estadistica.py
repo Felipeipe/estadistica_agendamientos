2f6b8cbb05422bf435d130f0b12868f891cb7749
Felipe González Poblete <gonzalezpobletefelipe@gmail.com>
2024-02-22 15:35:29 -0300

    se agrega nuevo módulo para contener las funciones que se habían creado, para mejores prácticas de programación

--- estadistica.py
+++ estadistica.py
@@ -1,56 +1,24 @@
+import func_est as func
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import os
 
 path=os.path.join(os.getcwd(),"datos")
 
-
-ventas_ingresadas:pd.DataFrame = pd.read_excel(os.path.join(path,"ventas_ingresadas.xlsx"),index_col=None)
-citas_creadas:pd.DataFrame     = pd.read_excel(os.path.join(path,"citas_creadas.xlsx"),index_col=None)
-cambio_estado:pd.DataFrame     = pd.read_excel(os.path.join(path,"cambios_de_estado_de_cita.xlsx"),index_col=None)
-dias_trabajados:pd.DataFrame   = pd.read_excel(os.path.join(path,"dias_trabajados.xlsx"),index_col=None)
-
+ventas_ingresadas:pd.DataFrame = func.lecturaDeExcels("ventas_ingresadas.xlsx",path)
+citas_creadas:pd.DataFrame     = func.lecturaDeExcels("citas_creadas.xlsx",path)
+cambio_estado:pd.DataFrame     = func.lecturaDeExcels("cambios_de_estado_de_cita.xlsx",path)
+dias_trabajados:pd.DataFrame   = func.lecturaDeExcels("dias_trabajados.xlsx",path)
 
 
-
-def difference(df:pd.DataFrame,dt:pd.DataFrame) -> pd.DataFrame:
-    """
-    Calcula la variación promedio entre dos columnas, ponderados por un DataFrame 
-    """
-    j=2
-    df0=df.copy()
-    df0[df0.columns[1]]=np.zeros(len(df0))
-    while j < len(df.columns):
-        df0[df0.columns[j]]=(df[df.columns[j]]-df[df.columns[j-1]])/dt[dt.columns[j]]
-        j+=1
-    return df0
-
-def graph(df:pd.DataFrame, titulo:str, xlabel:str, ylabel:str) -> None:
-    """Graficador de recepcionistas
-    """
-    plt.figure(figsize=(10, 6))
-    for i in range(len(df)):
-        plt.plot(df.columns[1:], df.iloc[i, 1:], marker='o', label=df['Recepcionista'][i])
-
-    plt.title(titulo)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.legend(loc='best')
-    plt.grid(True)
-    plt.show()
-    pass
-
-
-diff_ventas:pd.DataFrame = difference(ventas_ingresadas,dias_trabajados)
-diff_citas:pd.DataFrame  = difference(citas_creadas,dias_trabajados)
-diff_cambio:pd.DataFrame = difference(cambio_estado,dias_trabajados)
+diff_ventas:pd.DataFrame = func.difference(ventas_ingresadas,dias_trabajados)
+diff_citas:pd.DataFrame  = func.difference(citas_creadas,dias_trabajados)
+diff_cambio:pd.DataFrame = func.difference(cambio_estado,dias_trabajados)
 
 # Graficar datos de ventas
-graph(diff_ventas,"Ventas diarias promedio",'Fechas', 'N° de ventas')
+func.graphAll(diff_ventas,"Ventas diarias promedio",'Fechas', 'N° de ventas')
 
 # Graficar Citas creadas por secretario
-graph(diff_citas,"citas agendadas diariamente promedio",'Fechas', 'N° de citas')
+func.graphAll(diff_citas,"citas agendadas diariamente promedio",'Fechas', 'N° de citas')
 
 # Gráfico de datos de cambio de estado de citas
-graph(diff_cambio,"cambio de estado diario promedio",'Fechas','N° de cambios de estado')
+func.graphAll(diff_cambio,"cambio de estado diario promedio",'Fechas','N° de cambios de estado')
---
+++ func_est.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+def lecturaDeExcels(name:str,ruta:str)->pd.DataFrame:
+    """Lee un excelñ de una ruta en especifico, de un nombre 'name'
+    """
+    df=pd.read_excel(os.path.join(ruta,f"{name}"),index_col=None)
+    return df
+
+def difference(df:pd.DataFrame,dt:pd.DataFrame) -> pd.DataFrame:
+    """
+    Calcula la variación promedio entre dos columnas, ponderados por un DataFrame 
+    """
+    j=2
+    df0=df.copy()
+    df0[df0.columns[1]]=np.zeros(len(df0))
+    while j < len(df.columns):
+        df0[df0.columns[j]]=(df[df.columns[j]]-df[df.columns[j-1]])/dt[dt.columns[j]]
+        j+=1
+    return df0
+
+def graph(df:pd.DataFrame, recepcionista:str, titulo:str, xlabel:str, ylabel:str) -> None:
+    """Graficador de recepcionistas
+    """
+    plt.figure(figsize=(10, 6))
+    plt.plot(df.columns[1:], df.loc[f"{recepcionista}", 1:], marker='o', label=f"{recepcionista}")
+    plt.title(titulo)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend(loc='best')
+    plt.grid(True)
+    plt.show()
+    pass
+
+def graphAll(df:pd.DataFrame, titulo:str, xlabel:str, ylabel:str)->None:
+    """ Grafica los datos del DataFrame 'df' dentro de todos los 
+    
+    """
+    for i in range(len(df)):
+        plt.plot(df.columns[1:], df.iloc[i, 1:], marker='o', label=df['Recepcionista'][i])
+
+    plt.title(titulo)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend(loc='best')
+    plt.grid(True)
+    plt.show()
