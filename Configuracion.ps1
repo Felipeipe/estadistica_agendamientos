c723d3dac01caabbc940b11e8ff0385e805d6973
Felipeipe <gonzalezpobletefelipe@gmail.com>
2024-02-24 21:18:53 -0300

    Creación de paquete de instalación

--- Configuracion.ps1
+++ Configuracion.ps1
@@ -1,10 +1,10 @@
 ﻿# Instalar Python 3 usando Winget
 winget install --id=Python.Python.3.12 -e
 
-Write-Output "Python3 se ha instalado correctamente, se esperará 20 segundos para asegurar la correcta instalación"
+Write-Output "Python3 se ha instalado correctamente, se esperará 10 segundos para asegurar la correcta instalación"
 
 # Esperar un momento para asegurarse de que la instalación se complete
-Start-Sleep -Seconds 20
+Start-Sleep -Seconds 10
 
 # Instalar las dependencias de Python usando pip
 $pythonPath = (Get-Command python).Source
