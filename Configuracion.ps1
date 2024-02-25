58037ffc94bf1b2b75a621dc7e21582e4a43b5d6
Felipeipe <gonzalezpobletefelipe@gmail.com>
2024-02-24 21:18:16 -0300

    Creación de paquete de instalación

--- Configuracion.ps1
+++ Configuracion.ps1
@@ -1,7 +1,16 @@
-﻿pip3 install pandas
-pip3 install matplotlib
-pip3 install numpy
-pip3 install openpyxl
-pip3 install xlrd
-pip3 install xlwt
+﻿# Instalar Python 3 usando Winget
+winget install --id=Python.Python.3.12 -e
 
+Write-Output "Python3 se ha instalado correctamente, se esperará 20 segundos para asegurar la correcta instalación"
+
+# Esperar un momento para asegurarse de que la instalación se complete
+Start-Sleep -Seconds 20
+
+# Instalar las dependencias de Python usando pip
+$pythonPath = (Get-Command python).Source
+$pipPath = Join-Path (Split-Path $pythonPath) "Scripts\pip.exe"
+
+$packages = @("pandas", "matplotlib", "numpy", "openpyxl", "xlrd", "xlwt")
+foreach ($package in $packages) {
+    & $pipPath install $package
+}
---
+++ LEEME.md
@@ -0,0 +1,13 @@
+# Proyecto de estadistica 
+
+Como poder usarlo.
+
+## Prerrequisitos
+
+Primeramente, debes tener instalado Python en tu computador. Lo puedes instalar, dependiendo de su sistema operativo, aquí:
+
+[Windows](https://www.python.org/downloads/)
+
+[MacOS](https://www.python.org/downloads/macos/)
+
+Luego, debes ejecutar el script llamado ["configuracion"](Configuracion.ps1) (esto solamente funciona para los usuarios de Windows)
